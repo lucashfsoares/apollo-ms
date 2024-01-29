19e8beaef4ee13712368248433a8377acf2d1f7c
Lucas Soares <lucas.soares@mobiis.com.br>
2024-01-28 21:44:44 -0300

    add: first modules

---
+++ apps/usuarios/src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users.module';
+
+@Module({
+  imports: [UsersModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
